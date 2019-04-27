13619aee4c969d44662c43a156ada7c0579fbf42
romkatv <roman.perepelitsa@gmail.com>
2019-04-27 20:03:54 +0200

    address todo: don't leave stranded gitstatusd instances on exec

--- bash-prompt-example.sh
+++ bash-prompt-example.sh
@@ -27,76 +27,102 @@
 #   * Logging.
 #   * gitstatus_start options: -t and -m.
 #   * gitstatus_query options: -d and -c.
-#
-# In addition, running `exec bash` (or `exec anything` really) from the shell will
-# result in a stranded `gitstatusd` in the background.
-#
-# A more complete port of the gitstatus ZSH API to bash would be a most welcome
-# contribution.
 
-# Simplistic translation to bash of gitstatus_start from the official gitstatus ZSH API.
+# Partial translation to bash of gitstatus_start from the official gitstatus ZSH API.
 # See https://github.com/romkatv/gitstatus/blob/master/gitstatus.plugin.zsh for documentation.
 function gitstatus_start() {
-  [[ "$GITSTATUS_REQ_FD" -gt 0 && "$GITSTATUS_RESP_FD" -gt 0 ]] && return  # already started
+  [[ -z "${GITSTATUS_DAEMON_PID:-}" ]] || return 0  # already started
 
-  unset GITSTATUS_REQ_FD GITSTATUS_RESP_FD
+  local req_fifo resp_fifo
 
-  local daemon="$GITSTATUS_DAEMON"
-  if [[ -z "$daemon" ]]; then
-    local os   &&   os=$(uname -s)                    || return
-    local arch && arch=$(uname -m)                    || return
-    local dir  &&  dir=$(dirname "${BASH_SOURCE[0]}") || return
-    daemon="$dir/bin/gitstatusd-${os,,}-${arch,,}"
+  function gitstatus_start_impl() {
+    local daemon="${GITSTATUS_DAEMON:-}"
+    if [[ -z "$daemon" ]]; then
+      local os   &&   os=$(uname -s)                    || return
+      local arch && arch=$(uname -m)                    || return
+      local dir  &&  dir=$(dirname "${BASH_SOURCE[0]}") || return
+      daemon="$dir/bin/gitstatusd-${os,,}-${arch,,}"
+    fi
+
+    local threads="${GITSTATUS_NUM_THREADS:-0}"
+    if (( threads <= 0 )); then
+      case "$(uname -s)" in
+        FreeBSD) threads=$(sysctl -n hw.ncpu)         || return;;
+        *)       threads=$(getconf _NPROCESSORS_ONLN) || return;;
+      esac
+      (( threads *=  2 ))
+      (( threads >=  2 )) || threads=2
+      (( threads <= 32 )) || threads=32
+    fi
+
+    req_fifo=$(mktemp -u "${TMPDIR:-/tmp}"/gitstatus.$$.pipe.req.XXXXXXXXXX)   || return
+    resp_fifo=$(mktemp -u "${TMPDIR:-/tmp}"/gitstatus.$$.pipe.resp.XXXXXXXXXX) || return
+    mkfifo "$req_fifo" "$resp_fifo"                                            || return
+    exec {GITSTATUS_REQ_FD}<>"$req_fifo" {GITSTATUS_RESP_FD}<>"$resp_fifo"     || return
+    command rm "$req_fifo" "$resp_fifo"                                        || return
+
+    { <&$GITSTATUS_REQ_FD >&$GITSTATUS_RESP_FD 2>/dev/null bash -c "
+        ${daemon@Q} --sigwinch-pid=$$ --num-threads=$threads
+        echo -nE $'bye\x1f0\x1e'" & } 2>/dev/null
+    disown
+    GITSTATUS_DAEMON_PID=$!
+
+    local reply
+    echo -nE $'hello\x1f\x1e' >&$GITSTATUS_REQ_FD           || return
+    IFS='' read -rd $'\x1e' -u $GITSTATUS_RESP_FD -t5 reply || return
+    [[ "$reply" == $'hello\x1f0' ]]                         || return
+  }
+
+  if ! gitstatus_start_impl; then
+    echo "gitstatus failed to initialize" >&2
+    [[ -z "${GITSTATUS_REQ_FD:-}"     ]] || exec {GITSTATUS_REQ_FD}>&-
+    [[ -z "${GITSTATUS_RESP_FD:-}"    ]] || exec {GITSTATUS_RESP_FD}>&-
+    [[ -z "${req_fifo:-}"             ]] || command rm -f "$req_fifo"
+    [[ -z "${resp_fifo:-}"            ]] || command rm -f "$resp_fifo"
+    [[ -z "${GITSTATUS_DAEMON_PID:-}" ]] || kill -- -"$GITSTATUS_DAEMON_PID" &>/dev/null
+    unset GITSTATUS_DAEMON_PID GITSTATUS_REQ_FD GITSTATUS_RESP_FD
+    unset -f gitstatus_start_impl
+    return 1
   fi
 
-  [[ -f "$daemon" ]] || return
+  unset -f gitstatus_start_impl
 
-  local threads="$GITSTATUS_NUM_THREADS"
-  if [[ ! "$threads" -gt 0 ]]; then
-    case "$(uname -s)" in
-      FreeBSD) threads=$(sysctl -n hw.ncpu)         || return;;
-      *)       threads=$(getconf _NPROCESSORS_ONLN) || return;;
-    esac
-    (( threads *= 2 ))
-    [[ "$threads" -le 32 ]] || threads=32
-  fi
+  function _gitstatus_exec() {
+    if [[ $# -gt 0 && -n "${GITSTATUS_DAEMON_PID:-}" ]]; then
+      exec {GITSTATUS_REQ_FD}>&- {GITSTATUS_RESP_FD}>&-
+      kill -- -"$GITSTATUS_DAEMON_PID" &>/dev/null || true
+      unalias exec builtin &>/dev/null             || true
+      unset -f _gitstatus_exec _gitstatus_builtin
+      unset GITSTATUS_DAEMON_PID GITSTATUS_REQ_FD GITSTATUS_RESP_FD
+    fi
+    exec "$@"
+    local ret=$?
+    [[ -n "${GITSTATUS_DAEMON_PID:-}" ]] || gitstatus_start || true
+    return $ret
+  }
+  alias exec=_gitstatus_exec
 
-  local req_fifo resp_fifo daemon_pid reply                                    &&
-    req_fifo=$(mktemp -u "${TMPDIR:-/tmp}"/gitstatus.$$.pipe.req.XXXXXXXXXX)   &&
-    resp_fifo=$(mktemp -u "${TMPDIR:-/tmp}"/gitstatus.$$.pipe.resp.XXXXXXXXXX) &&
-    mkfifo "$req_fifo" "$resp_fifo"                                            &&
-    exec {GITSTATUS_REQ_FD}<>"$req_fifo" {GITSTATUS_RESP_FD}<>"$resp_fifo"     &&
-    rm "$req_fifo" "$resp_fifo"                                                &&
-    { "$daemon" --sigwinch-pid=$$ --num-threads="$threads" \
-        <&$GITSTATUS_REQ_FD >&$GITSTATUS_RESP_FD 2>/dev/null & }               &&
-    disown %1                                                                  &&
-    daemon_pid=$!                                                              &&
-    echo -nE $'hello\x1f\x1e' >&$GITSTATUS_REQ_FD                              &&
-    IFS='' read -rd $'\x1e' -u $GITSTATUS_RESP_FD -t5 reply                    &&
-    [[ "$reply" == $'hello\x1f0' ]] || {
-      [[ "$daemon_pid"        -gt 0 ]] && kill "$daemon_pid" &>/dev/null
-      [[ "$GITSTATUS_REQ_FD"  -gt 0 ]] && exec {GITSTATUS_REQ_FD}>&-
-      [[ "$GITSTATUS_RESP_FD" -gt 0 ]] && exec {GITSTATUS_RESP_FD}>&-
-      [[ -n "$req_fifo"             ]] && rm -f "$req_fifo"
-      [[ -n "$resp_fifo"            ]] && rm -f "$resp_fifo"
-      unset GITSTATUS_REQ_FD GITSTATUS_RESP_FD
-      return 1
-    }
+  function _gitstatus_builtin() {
+    while [[ "${1:-}" == builtin ]]; do shift; done
+    [[ "${1:-}" != exec ]] || set -- _gitstatus_exec "${@:2}"
+    "$@"
+  }
+  alias builtin=_gitstatus_builtin
 }
 
-# Simplistic translation to bash of gitstatus_query from the official gitstatus ZSH API.
+# Partial translation to bash of gitstatus_query from the official gitstatus ZSH API.
 # See https://github.com/romkatv/gitstatus/blob/master/gitstatus.plugin.zsh for documentation.
 function gitstatus_query() {
-  [[ "$GITSTATUS_REQ_FD" -gt 0 && "$GITSTATUS_RESP_FD" -gt 0 ]] || return  # not started
+  [[ -n "$GITSTATUS_DAEMON_PID" ]] || return  # not started
 
   local req_id="$RANDOM.$RANDOM.$RANDOM.$RANDOM"
   local dir="${GIT_DIR:-$PWD}"
   [[ "$dir" == /* ]] || dir="$PWD/$dir"
-  echo -nE "$req_id"$'\x1f'"$dir"$'\x1e' >&$GITSTATUS_REQ_FD || return  # error
+  echo -nE "$req_id"$'\x1f'"$dir"$'\x1e' >&$GITSTATUS_REQ_FD || return
 
   local -a resp
   while true; do
-    IFS=$'\x1f' read "$@" -rd $'\x1e' -a resp -u $GITSTATUS_RESP_FD || return  # error
+    IFS=$'\x1f' read "$@" -rd $'\x1e' -a resp -u $GITSTATUS_RESP_FD || return
     [[ "${resp[0]}" == "$req_id" ]] && break
   done
 
