4c4e14850530c9dd3177738d74ca7a035c731ec2
romkatv <roman.perepelitsa@gmail.com>
2019-04-28 12:36:53 +0200

    add flags; write docs

--- bash-prompt-example.sh
+++ bash-prompt-example.sh
@@ -4,37 +4,31 @@
 #
 #   git clone https://github.com/romkatv/gitstatus.git ~/gitstatus
 #   echo 'source ~/gitstatus/bash-prompt-example.sh' >> ~/.bashrc
-#
-# Example prompt:
-#
-#   ~/powerlevel10k master+!? ⇡2 ⇣3 *4
-#   ❯
-#
-# Meaning:
-#
-#   * ~/powerlevel10k -- current directory
-#   * master          -- current git branch
-#   * +               -- git repo has changes staged for commit
-#   * !               -- git repo has unstaged changes
-#   * ?               -- git repo has untracked files
-#   * ⇡2              -- local branch is ahead of origin by 2 commits
-#   * ⇣3              -- local branch is behind origin by 3 commits
-#   * *4              -- git repo has 4 stashes
-#
-# This code uses `gitstatusd` directly, whose API has no backward compatibility
-# guarantees.
-#
-# Missing features compared to the official ZSH API provided by gitstatus:
-#
-#   * Async API.
-#   * Multiple gitstatusd instances.
-#   * Logging.
-#   * gitstatus_start options: -t and -m.
-#   * gitstatus_query options: -d and -c.
 
-# Partial translation to bash of gitstatus_start from the official gitstatus ZSH API.
-# See https://github.com/romkatv/gitstatus/blob/master/gitstatus.plugin.zsh for documentation.
+[[ $- == *i* ]] || return  # non-interactive shell
+
+# Starts gitstatusd in the background. Does nothing and succeeds if gitstatusd
+# is already running.
+#
+# Usage: gitstatus_start [OPTION]...
+#
+#   -t FLOAT  Fail the self-check on initialization if not getting a response from
+#             gitstatusd for this this many seconds. Defaults to 5.
+#   -m INT    Report -1 unstaged and untracked if there are more than this many files
+#             in the index. Negative value means infinity. Defaults to -1.
 function gitstatus_start() {
+  unset OPTIND
+  local opt timeout=5 max_dirty=-1
+  while getopts "t:m:" opt; do
+    case "$opt" in
+      t) timeout=$OPTARG;;
+      m) max_dirty=$OPTARG;;
+      *) return 1;;
+    esac
+  done
+
+  (( OPTIND == $# + 1 )) || { echo "usage: gitstatus_start [OPTION]..." >&2; return 1; }
+
   [[ -z "${GITSTATUS_DAEMON_PID:-}" ]] || return 0  # already started
 
   local req_fifo resp_fifo
@@ -65,68 +59,136 @@ function gitstatus_start() {
     exec {GITSTATUS_REQ_FD}<>"$req_fifo" {GITSTATUS_RESP_FD}<>"$resp_fifo"     || return
     command rm "$req_fifo" "$resp_fifo"                                        || return
 
-    { <&$GITSTATUS_REQ_FD >&$GITSTATUS_RESP_FD 2>/dev/null bash -c "
-        ${daemon@Q} --sigwinch-pid=$$ --num-threads=$threads
+    if [[ "${GITSTATUS_ENABLE_LOGGING:-0}" == 1 ]]; then
+      GITSTATUS_DAEMON_LOG=$(mktemp "${TMPDIR:-/tmp}"/gitstatus.$$.log.XXXXXXXXXX) || return
+    else
+      GITSTATUS_DAEMON_LOG=/dev/null
+    fi
+
+    { <&$GITSTATUS_REQ_FD >&$GITSTATUS_RESP_FD 2>"$GITSTATUS_DAEMON_LOG" bash -c "
+        trap 'kill %1 &>/dev/null' SIGINT SIGTERM EXIT
+        ${daemon@Q}                             \
+          --sigwinch-pid=$$                     \
+          --num-threads=${threads@Q}            \
+          --dirty-max-index-size=${max_dirty@Q} \
+          0<&0 1>&1 2>&2 &
+        wait
         echo -nE $'bye\x1f0\x1e'" & } 2>/dev/null
     disown
     GITSTATUS_DAEMON_PID=$!
 
     local reply
-    echo -nE $'hello\x1f\x1e' >&$GITSTATUS_REQ_FD           || return
-    IFS='' read -rd $'\x1e' -u $GITSTATUS_RESP_FD -t5 reply || return
-    [[ "$reply" == $'hello\x1f0' ]]                         || return
+    echo -nE $'hello\x1f\x1e' >&$GITSTATUS_REQ_FD                     || return
+    IFS='' read -rd $'\x1e' -u $GITSTATUS_RESP_FD -t "$timeout" reply || return
+    [[ "$reply" == $'hello\x1f0' ]]                                   || return
   }
 
   if ! gitstatus_start_impl; then
-    echo "gitstatus failed to initialize" >&2
-    [[ -z "${GITSTATUS_REQ_FD:-}"     ]] || exec {GITSTATUS_REQ_FD}>&-
-    [[ -z "${GITSTATUS_RESP_FD:-}"    ]] || exec {GITSTATUS_RESP_FD}>&-
-    [[ -z "${req_fifo:-}"             ]] || command rm -f "$req_fifo"
-    [[ -z "${resp_fifo:-}"            ]] || command rm -f "$resp_fifo"
-    [[ -z "${GITSTATUS_DAEMON_PID:-}" ]] || kill -- -"$GITSTATUS_DAEMON_PID" &>/dev/null
-    unset GITSTATUS_DAEMON_PID GITSTATUS_REQ_FD GITSTATUS_RESP_FD
+    echo "gitstatus_start: failed to start gitstatusd" >&2
+    [[ -z "${req_fifo:-}"  ]] || command rm -f "$req_fifo"
+    [[ -z "${resp_fifo:-}" ]] || command rm -f "$resp_fifo"
     unset -f gitstatus_start_impl
+    gitstatus_stop
     return 1
   fi
 
   unset -f gitstatus_start_impl
 
-  function _gitstatus_exec() {
-    if [[ $# -gt 0 && -n "${GITSTATUS_DAEMON_PID:-}" ]]; then
-      exec {GITSTATUS_REQ_FD}>&- {GITSTATUS_RESP_FD}>&-
-      kill -- -"$GITSTATUS_DAEMON_PID" &>/dev/null || true
-      unalias exec builtin &>/dev/null             || true
-      unset -f _gitstatus_exec _gitstatus_builtin
-      unset GITSTATUS_DAEMON_PID GITSTATUS_REQ_FD GITSTATUS_RESP_FD
-    fi
-    exec "$@"
-    local ret=$?
-    [[ -n "${GITSTATUS_DAEMON_PID:-}" ]] || gitstatus_start || true
-    return $ret
-  }
-  alias exec=_gitstatus_exec
+  if [[ "${GITSTATUS_STOP_ON_EXEC:-1}" == 1 ]]; then
+    function _gitstatus_exec() {
+      (( ! $# )) || gitstatus_stop
+      local ret=0
+      exec "$@" || ret=$?
+      [[ -n "${GITSTATUS_DAEMON_PID:-}" ]] || gitstatus_start || true
+      return $ret
+    }
+    alias exec=_gitstatus_exec
 
-  function _gitstatus_builtin() {
-    while [[ "${1:-}" == builtin ]]; do shift; done
-    [[ "${1:-}" != exec ]] || set -- _gitstatus_exec "${@:2}"
-    "$@"
-  }
-  alias builtin=_gitstatus_builtin
+    function _gitstatus_builtin() {
+      while [[ "${1:-}" == builtin ]]; do shift; done
+      [[ "${1:-}" != exec ]] || set -- _gitstatus_exec "${@:2}"
+      "$@"
+    }
+    alias builtin=_gitstatus_builtin
+
+    _GITSTATUS_EXEC_HOOK=1
+  else
+    unset _GITSTATUS_EXEC_HOOK
+  fi
 }
 
-# Partial translation to bash of gitstatus_query from the official gitstatus ZSH API.
-# See https://github.com/romkatv/gitstatus/blob/master/gitstatus.plugin.zsh for documentation.
+# Stops gitstatusd if it's running.
+function gitstatus_stop() {
+  [[ -z "${GITSTATUS_REQ_FD:-}"     ]] || exec {GITSTATUS_REQ_FD}>&-               || true
+  [[ -z "${GITSTATUS_RESP_FD:-}"    ]] || exec {GITSTATUS_RESP_FD}>&-              || true
+  [[ -z "${GITSTATUS_DAEMON_PID:-}" ]] || kill "$GITSTATUS_DAEMON_PID" &>/dev/null || true
+  [[ -z "${_GITSTATUS_EXEC_HOOK:-}" ]] || unalias exec builtin &>/dev/null         || true
+  unset GITSTATUS_REQ_FD GITSTATUS_RESP_FD GITSTATUS_DAEMON_PID _GITSTATUS_EXEC_HOOK
+  unset -f _gitstatus_exec _gitstatus_builtin
+}
+
+# Retrives status of a git repository from a directory under its working tree.
+#
+# Usage: gitstatus_query [OPTION]...
+#
+#   -d STR    Directory to query. Defaults to ${GIT_DIR:-$PWD}.
+#   -t FLOAT  Timeout in seconds. Will block for at most this long. If no results
+#             are available by then, will return error.
+#
+# On success sets VCS_STATUS_RESULT to one of the following values:
+#
+#   norepo-sync  The directory doesn't belong to a git repository.
+#   ok-sync      The directory belongs to a git repository.
+#
+# If VCS_STATUS_RESULT is ok-sync, additional variables are set:
+#
+#   VCS_STATUS_WORKDIR         Git repository working directory. Not empty.
+#   VCS_STATUS_COMMIT          Commit hash that HEAD is pointing to. Either 40 hex digits
+#                              or empty if there is no HEAD (empty repository).
+#   VCS_STATUS_LOCAL_BRANCH    Local branch name or empty if not on a branch.
+#   VCS_STATUS_REMOTE_NAME     The remote name, e.g. "upstream" or "origin".
+#   VCS_STATUS_REMOTE_BRANCH   Upstream branch name. Can be empty.
+#   VCS_STATUS_REMOTE_URL      Remote URL. Can be empty.
+#   VCS_STATUS_ACTION          Repository state, A.K.A. action. Can be empty.
+#   VCS_STATUS_HAS_STAGED      1 if there are staged changes, 0 otherwise.
+#   VCS_STATUS_HAS_UNSTAGED    1 if there are unstaged changes, 0 if there aren't, -1 if
+#                              unknown.
+#   VCS_STATUS_HAS_UNTRACKED   1 if there are untracked files, 0 if there aren't, -1 if
+#                              unknown.
+#   VCS_STATUS_COMMITS_AHEAD   Number of commits the current branch is ahead of upstream.
+#                              Non-negative integer.
+#   VCS_STATUS_COMMITS_BEHIND  Number of commits the current branch is behind upstream.
+#                              Non-negative integer.
+#   VCS_STATUS_STASHES         Number of stashes. Non-negative integer.
+#   VCS_STATUS_TAG             The last tag (in lexicographical order) that points to the
+#                              same commit as HEAD.
+#
+# The point of reporting -1 as unstaged and untracked is to allow the command to skip
+# scanning files in large repos. See -m flag of gitstatus_start.
+#
+# gitstatus_query returns an error if gitstatus_start hasn't been called in the same
+# shell or the call had failed.
 function gitstatus_query() {
+  unset OPTIND
+  local opt dir="${GIT_DIR:-$PWD}" timeout=()
+  while getopts "d:c:t:" opt "$@"; do
+    case "$opt" in
+      d) dir=$OPTARG;;
+      t) timeout=(-t "$OPTARG");;
+      *) return 1;;
+    esac
+  done
+  (( OPTIND == $# + 1 )) || { echo "usage: gitstatus_query [OPTION]..." >&2; return 1; }
+
   [[ -n "$GITSTATUS_DAEMON_PID" ]] || return  # not started
 
   local req_id="$RANDOM.$RANDOM.$RANDOM.$RANDOM"
-  local dir="${GIT_DIR:-$PWD}"
   [[ "$dir" == /* ]] || dir="$PWD/$dir"
   echo -nE "$req_id"$'\x1f'"$dir"$'\x1e' >&$GITSTATUS_REQ_FD || return
 
   local -a resp
   while true; do
-    IFS=$'\x1f' read "$@" -rd $'\x1e' -a resp -u $GITSTATUS_RESP_FD || return
+    IFS=$'\x1f' read -rd $'\x1e' -a resp -u $GITSTATUS_RESP_FD "${timeout[@]}" || return
     [[ "${resp[0]}" == "$req_id" ]] && break
   done
 
@@ -145,7 +207,7 @@ function gitstatus_query() {
     VCS_STATUS_COMMITS_AHEAD="${resp[12]}"
     VCS_STATUS_COMMITS_BEHIND="${resp[13]}"
     VCS_STATUS_STASHES="${resp[14]}"
-    VCS_STATUS_TAG="${resp[15]}"
+    VCS_STATUS_TAG="${resp[15]:-}"
   else
     VCS_STATUS_RESULT=norepo-sync
     unset VCS_STATUS_WORKDIR
@@ -165,15 +227,27 @@ function gitstatus_query() {
   fi
 }
 
-# This is an example showing how gitstatus_query can be used to put git info into PS1.
+# Sets GITSTATUS_PROMPT to reflect the state of the current git repository.
+# The value is empty if not in a git repository. Forwards all arguments to
+# gitstatus_query.
 #
-# Sets GITSTATUS_PROMPT to reflect the state of the current git repository (empty if not in
-# a git repository).
-function update_git_prompt() {
+# Example value of GITSTATUS_PROMPT:
+#
+#   master+!? ⇡2 ⇣3 *4
+#
+# Meaning:
+#
+#   master   current branch
+#   +        git repo has changes staged for commit
+#   !        git repo has unstaged changes
+#   ?        git repo has untracked files
+#   ⇡2       local branch is ahead of origin by 2 commits
+#   ⇣3       local branch is behind origin by 3 commits
+#   *4       git repo has 4 stashes
+function gitstatus_prompt_update() {
   GITSTATUS_PROMPT=""
 
-  # Call gitstatus_query synchronously with 5s timeout.
-  gitstatus_query -t 5                  || return 1  # error
+  gitstatus_query "$@"                  || return 1  # error
   [[ "$VCS_STATUS_RESULT" == ok-sync ]] || return 0  # not a git repo
 
   local     reset=$'\e[0m'         # no color
@@ -203,8 +277,19 @@ function update_git_prompt() {
 }
 
 # Start gitstatusd in the background.
-gitstatus_start
+gitstatus_stop && gitstatus_start
 
-shopt -s promptvars
-PROMPT_COMMAND=update_git_prompt
-PS1='\w${GITSTATUS_PROMPT:+ }${GITSTATUS_PROMPT}\n❯ '
+# On every prompt, fetch git status with 5s timeout and set GITSTATUS_PROMPT.
+PROMPT_COMMAND='gitstatus_prompt_update -t 5'
+
+# Customize prompt. Put $GITSTATUS_PROMPT in it reflect git status.
+#
+# Example:
+#
+#   user@host ~/projects/skynet master+!
+#   $ █
+PS1='\[\033[01;32m\]\u@\h\[\033[00m\] '           # green user@host
+PS1+='\[\033[01;34m\]\w\[\033[00m\]'              # blue current working directory
+PS1+='${GITSTATUS_PROMPT:+ }${GITSTATUS_PROMPT}'  # git status
+PS1+='\n\[\033[01;$((31+!$?))m\]\$\[\033[00m\] '  # green/red (success/error) $/# (normal/root)
+PS1+='\[\e]0;\u@\h: \w\a\]'                       # terminal title: user@host: dir
