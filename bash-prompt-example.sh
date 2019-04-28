7258c421e12518ed43a73be8ecb626b7d75b7bd9
romkatv <roman.perepelitsa@gmail.com>
2019-04-28 12:46:04 +0200

    comments

--- bash-prompt-example.sh
+++ bash-prompt-example.sh
@@ -290,6 +290,6 @@ PROMPT_COMMAND=gitstatus_prompt_update
 #   $ █
 PS1='\[\033[01;32m\]\u@\h\[\033[00m\] '           # green user@host
 PS1+='\[\033[01;34m\]\w\[\033[00m\]'              # blue current working directory
-PS1+='${GITSTATUS_PROMPT:+ }${GITSTATUS_PROMPT}'  # git status
+PS1+='${GITSTATUS_PROMPT:+ }${GITSTATUS_PROMPT}'  # git status (requires promptvars option)
 PS1+='\n\[\033[01;$((31+!$?))m\]\$\[\033[00m\] '  # green/red (success/error) $/# (normal/root)
 PS1+='\[\e]0;\u@\h: \w\a\]'                       # terminal title: user@host: dir
