c685c4c03795ebe5d70920bcfbc5c808cc8feb82
romkatv <roman.perepelitsa@gmail.com>
2019-04-28 15:23:42 +0200

    docs

--- gitstatus.plugin.zsh
+++ gitstatus.plugin.zsh
@@ -14,6 +14,33 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with GitStatus. If not, see <https://www.gnu.org/licenses/>.
+#
+# ------------------------------------------------------------------
+#
+# Example: Start gitstatusd, send it a request, wait for response and print it.
+#
+#   source gitstatus.plugin.zsh
+#   gitstatus_start MY
+#   gitstatus_query -d $PWD MY
+#   set | egrep '^VCS_STATUS'
+#
+# Output:
+#
+#   VCS_STATUS_ACTION=''
+#   VCS_STATUS_COMMIT=6e86ec135bf77875e222463cbac8ef72a7e8d823
+#   VCS_STATUS_COMMITS_AHEAD=0
+#   VCS_STATUS_COMMITS_BEHIND=0
+#   VCS_STATUS_HAS_STAGED=0
+#   VCS_STATUS_HAS_UNSTAGED=1
+#   VCS_STATUS_HAS_UNTRACKED=1
+#   VCS_STATUS_LOCAL_BRANCH=master
+#   VCS_STATUS_REMOTE_BRANCH=master
+#   VCS_STATUS_REMOTE_NAME=origin
+#   VCS_STATUS_REMOTE_URL=git@github.com:romkatv/powerlevel10k.git
+#   VCS_STATUS_RESULT=ok-sync
+#   VCS_STATUS_STASHES=0
+#   VCS_STATUS_TAG=''
+#   VCS_STATUS_WORKDIR=/home/romka/.oh-my-zsh/custom/themes/powerlevel10k
 
 [[ -o interactive ]] || return
  autoload -Uz add-zsh-hook && zmodload zsh/datetime && zmodload zsh/system || return
