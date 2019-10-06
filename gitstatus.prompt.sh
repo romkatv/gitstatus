dcc232613b947952c9f8fb845cff0387dacca331
romkatv <roman.perepelitsa@gmail.com>
2019-10-06 18:17:05 +0200

    use the same git status format in bash as in zsh

--- gitstatus.prompt.sh
+++ gitstatus.prompt.sh
@@ -42,30 +42,47 @@ function gitstatus_prompt_update() {
   gitstatus_query "$@"                  || return 1  # error
   [[ "$VCS_STATUS_RESULT" == ok-sync ]] || return 0  # not a git repo
 
-  local     reset=$'\e[0m'         # no color
-  local     clean=$'\e[38;5;076m'  # green foreground
-  local untracked=$'\e[38;5;014m'  # teal foreground
-  local  modified=$'\e[38;5;011m'  # yellow foreground
+  local      reset=$'\e[0m'         # no color
+  local      clean=$'\e[38;5;076m'  # green foreground
+  local  untracked=$'\e[38;5;014m'  # teal foreground
+  local   modified=$'\e[38;5;011m'  # yellow foreground
+  local conflicted=$'\e[38;5;196m'  # red foreground
 
   local p
-  if (( VCS_STATUS_HAS_STAGED || VCS_STATUS_HAS_UNSTAGED )); then
-    p+="$modified"
-  elif (( VCS_STATUS_HAS_UNTRACKED )); then
-    p+="$untracked"
+
+  local where  # branch name, tag or commit
+  if [[ -n "$VCS_STATUS_LOCAL_BRANCH" ]]; then
+    where="$VCS_STATUS_LOCAL_BRANCH"
+  elif [[ -n "$VCS_STATUS_TAG" ]]; then
+    p+="${reset}#"
+    where="$VCS_STATUS_TAG"
   else
-    p+="$clean"
+    p+="${reset}@"
+    where="${VCS_STATUS_COMMIT:0:10}"
   fi
-  p+="${VCS_STATUS_LOCAL_BRANCH:-@${VCS_STATUS_COMMIT}}"
 
-  [[ -n "$VCS_STATUS_TAG"               ]] && p+="#${VCS_STATUS_TAG}"
-  [[ "$VCS_STATUS_HAS_STAGED"      == 1 ]] && p+="${modified}+"
-  [[ "$VCS_STATUS_HAS_UNSTAGED"    == 1 ]] && p+="${modified}!"
-  [[ "$VCS_STATUS_HAS_UNTRACKED"   == 1 ]] && p+="${untracked}?"
-  [[ "$VCS_STATUS_COMMITS_AHEAD"  -gt 0 ]] && p+="${clean} ⇡${VCS_STATUS_COMMITS_AHEAD}"
-  [[ "$VCS_STATUS_COMMITS_BEHIND" -gt 0 ]] && p+="${clean} ⇣${VCS_STATUS_COMMITS_BEHIND}"
-  [[ "$VCS_STATUS_STASHES"        -gt 0 ]] && p+="${clean} *${VCS_STATUS_STASHES}"
+  (( ${#where} > 32 )) && where="${where:0:12}…${where: -12}"  # truncate long branch names and tags
+  p+="${clean}${where}"
 
-  GITSTATUS_PROMPT="${reset}${p}${reset}"
+  # ⇣42 if behind the remote.
+  (( VCS_STATUS_COMMITS_BEHIND )) && p+=" ${clean}⇣${VCS_STATUS_COMMITS_BEHIND}"
+  # ⇡42 if ahead of the remote; no leading space if also behind the remote: ⇣42⇡42.
+  (( VCS_STATUS_COMMITS_AHEAD && !VCS_STATUS_COMMITS_BEHIND )) && p+=" "
+  (( VCS_STATUS_COMMITS_AHEAD  )) && p+="${clean}⇡${VCS_STATUS_COMMITS_AHEAD}"
+  # *42 if have stashes.
+  (( VCS_STATUS_STASHES        )) && p+=" ${clean}*${VCS_STATUS_STASHES}"
+  # 'merge' if the repo is in an unusual state.
+  [[ -n $VCS_STATUS_ACTION     ]] && p+=" ${conflicted}${VCS_STATUS_ACTION}"
+  # ~42 if have merge conflicts.
+  (( VCS_STATUS_NUM_CONFLICTED )) && p+=" ${conflicted}~${VCS_STATUS_NUM_CONFLICTED}"
+  # +42 if have staged changes.
+  (( VCS_STATUS_NUM_STAGED     )) && p+=" ${modified}+${VCS_STATUS_NUM_STAGED}"
+  # !42 if have unstaged changes.
+  (( VCS_STATUS_NUM_UNSTAGED   )) && p+=" ${modified}!${VCS_STATUS_NUM_UNSTAGED}"
+  # ?42 if have untracked files. It's really a question mark, your font isn't broken.
+  (( VCS_STATUS_NUM_UNTRACKED  )) && p+=" ${untracked}?${VCS_STATUS_NUM_UNTRACKED}"
+
+  GITSTATUS_PROMPT="${p}${reset}"
 }
 
 # Start gitstatusd in the background.
