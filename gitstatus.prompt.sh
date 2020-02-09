e727398c1934a8d0cf2468e71edb66a25f035c0b
romkatv <roman.perepelitsa@gmail.com>
2020-02-09 16:35:09 +0100

    remove boilerplate legalese from shell scripts; the original copyright and license still apply

--- gitstatus.prompt.sh
+++ gitstatus.prompt.sh
@@ -1,19 +1,4 @@
-# Copyright 2019 Roman Perepelitsa.
-#
-# This file is part of GitStatus. It sets Bash PS1 parameter to reflect git status.
-#
-# GitStatus is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# GitStatus is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with GitStatus. If not, see <https://www.gnu.org/licenses/>.
+# Simple Bash prompt with Git status.
 
 # Source gitstatus.plugin.sh from $GITSTATUS_DIR or from the same directory
 # in which the current script resides if the variable isn't set.
